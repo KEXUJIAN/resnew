32c2003ed3ecb372bbe129a2c5dd263bff7807f8
KEXUJIAN <1043736801@qq.com>
2017-11-26 13:31:05 +0800

    Database class initialize

--- application/libraries/MyPDO.php
+++ application/libraries/MyPDO.php
@@ -1,5 +1,6 @@
 <?php
 namespace Res\Util;
+
 use \PDO;
 
 /**
@@ -14,5 +15,32 @@ class MyPDO
         $this->pdo = new PDO($dsn, $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
         $this->pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->pdo->->setAttribute(PDO::ATTR_CASE, PDO::CASE_NATURAL);
+    }
+
+    public function beginTransaction()
+    {
+        return $this->pdo->beginTransaction();
+    }
+
+    public function inTransaction()
+    {
+        return $this->pdo->inTransaction();
+    }
+
+    public function commit()
+    {
+        return $this->pdo->commit();
+    }
+
+    public function rollBack()
+    {
+        return $this->pdo->rollBack();
+    }
+
+    public function prepare(string $sql)
+    {
+        return $this->pdo->prepare($sql);
     }
 }
