654923337f397851154b304d2e2eb74385529073
KEXUJIAN <1043736801@qq.com>
2017-12-23 20:27:56 +0800

    include some dependencies footer html

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,4 +1,23 @@
-<?php if ($display ?? true): ?>
-<?php endif; ?>
+    </div>
+    <?php if ($display ?? true): ?>
+    <footer class="navbar navbar-inverse navbar-fixed-bottom">
+        <div class="container">
+            <div class="navbar-header">
+                <a class="navbar-brand">Simple Resource Management Web System</a>
+            </div>
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a href="javasricpt:;"><i class="fa fa-copyright"></i> License: MIT </a>
+                </li>
+                <li>
+                    <a href="http://codeigniter.org.cn/" target="_blank">Powered by <i class="fa fa-fire"></i> CodeIgniter</a>
+                </li>
+                <li>
+                    <a href="http://fontawesome.io" target="_blank">Font Awesome <i class="fa fa-font-awesome"></i> by Dave Gandy</a>
+                </li>
+            </ul>
+        </div>
+    </footer>
+    <?php endif; ?>
 </body>
 </html>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,7 +4,31 @@
     <meta charset="UTF-8">
     <title><?php echo $title ?? '' ?></title>
     <link href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="https://cdn.bootcss.com/jquery-toast-plugin/1.3.2/jquery.toast.min.css" rel="stylesheet">
     <script src="https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://cdn.bootcss.com/jquery-toast-plugin/1.3.2/jquery.toast.min.js"></script>
     <script src="/asset/app.f3678996a21c4464153d.js"></script>
+    <style type="text/css">
+        body {
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            overflow: hidden;
+        }
+        .res-container {
+            position: absolute;
+            top: 50px;
+            bottom: 60px;
+            left: 0;
+            right: 0;
+            overflow: auto;
+        }
+    </style>
 </head>
+<body>
+
+    <div class="res-container">
